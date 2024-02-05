2f3fd147e59cee1557dd65bbbe41876c7b1666c5
tibor.kovacs <tibor.kovacs@strivacity.com>
2024-02-05 15:49:52 +0100

    Initial Commit

---
+++ garage-ios/garage_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  garage_iosApp.swift
+//  garage-ios
+//
+//  Created by Tibor Kovacs on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct garage_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
